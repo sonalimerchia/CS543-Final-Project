125412418cfa018323ecaa95f85ac805a1442133
ajs11 <ajs11@illinois.edu>
2024-05-07 15:42:54 -0500

    Final Code

--- model.py
+++ model.py
@@ -24,35 +24,24 @@ class Layer:
     def save(self, directory):
         pass
 
-# a base class for activation layers
-class Activation(Layer):
-    def __init__(self, function, function_derivative):
-        self.function = function
-        self.function_derivative = function_derivative
-    def forward(self, input):
-        self.input = input
-        return self.function(self.input)
-    def backward(self, output_grad, learn_rate):
-        return output_grad * self.function_derivative(self.input)
-
 # a convolutional layer that maintains the width and height of the data provided as input
 class ConvolutionalSame(Layer):
-    def __init__(self, input_shape, kernel_size, depth, directory=None):
+    def __init__(self, input_shape, kernel_size, out_channels, directory=None):
         self.input_shape = input_shape
-        self.input_depth = input_shape[0]
+        self.in_channels = input_shape[0]
         self.kernel_size = kernel_size
-        self.depth = depth
-        self.kernel_shape = (depth, self.input_depth, kernel_size, kernel_size)
+        self.out_channels = out_channels
+        self.kernel_shape = (out_channels, self.in_channels, kernel_size, kernel_size)
         self.kernels = np.random.rand(*self.kernel_shape) - 0.5
-        self.biases = np.random.rand(depth, input_shape[1], input_shape[2]) - 0.5
+        self.biases = np.random.rand(out_channels, input_shape[1], input_shape[2]) - 0.5
         if directory is not None:
             self.kernels = np.load(directory + '/kernels.pickle', allow_pickle=True)
             self.biases = np.load(directory + '/biases.pickle', allow_pickle=True)
     def forward(self, input):
         self.input = input
-        output = np.copy(self.biases)
-        for i in range(self.depth):
-            for j in range(self.input_depth):
+        output = self.biases.copy()
+        for i in range(self.out_channels):
+            for j in range(self.in_channels):
                 output[i] += signal.correlate2d(self.input[j], self.kernels[i, j], mode='same')
         return output
     def backward(self, output_grad, learn_rate):
@@ -60,8 +49,8 @@ class ConvolutionalSame(Layer):
         input_grad = np.zeros(self.input_shape)
         tmp_corr = np.zeros((self.kernel_size, self.kernel_size))
         tmp_corr[(self.kernel_size - 1) // 2:(self.kernel_size - 1) // 2] = 1
-        for i in range(self.depth):
-            for j in range(self.input_depth):
+        for i in range(self.out_channels):
+            for j in range(self.in_channels):
                 kernel_grad[i, j] = signal.correlate2d(signal.correlate2d(self.input[j], tmp_corr, mode='full'), output_grad[i], mode='valid')
                 input_grad[j] += signal.convolve2d(output_grad[i], self.kernels[i, j], mode='same')
         self.kernels -= learn_rate * kernel_grad
@@ -77,34 +66,34 @@ class ConvolutionalSame(Layer):
 
 # a transpose convolutional layer assuming valid convolution and stride equal to kernel size
 class ConvolutionalTranspose(Layer):
-    def __init__(self, output_shape, kernel_size, depth, directory=None):
+    def __init__(self, output_shape, kernel_size, in_channels, directory=None):
         self.output_shape = output_shape
-        self.output_depth = output_shape[0]
+        self.out_channels = output_shape[0]
         self.output_height = output_shape[1]
         self.output_width = output_shape[2]
         self.kernel_size = kernel_size
-        self.depth = depth
-        self.kernel_shape = (depth, self.output_depth, kernel_size, kernel_size)
-        self.input_shape = (depth, self.output_height // kernel_size, self.output_width // kernel_size)
+        self.in_channels = in_channels
+        self.kernel_shape = (in_channels, self.out_channels, kernel_size, kernel_size)
+        self.input_shape = (in_channels, self.output_height // kernel_size, self.output_width // kernel_size)
         self.kernels = np.random.rand(*self.kernel_shape) - 0.5
         self.biases = np.random.rand(*self.output_shape) - 0.5
         if directory is not None:
             self.kernels = np.load(directory + '/kernels.pickle', allow_pickle=True)
             self.biases = np.load(directory + '/biases.pickle', allow_pickle=True)
     def forward(self, input):
-        input_tmp = np.zeros((self.depth, self.output_height + self.kernel_size - 1, self.output_width + self.kernel_size - 1))
+        input_tmp = np.zeros((self.in_channels, self.output_height + self.kernel_size - 1, self.output_width + self.kernel_size - 1))
         input_tmp[:, self.kernel_size - 1::self.kernel_size, self.kernel_size - 1::self.kernel_size] = input
         self.input = input_tmp
         output = self.biases.copy()
-        for i in range(self.depth):
-            for j in range(self.output_depth):
+        for i in range(self.in_channels):
+            for j in range(self.out_channels):
                 output[j] += signal.convolve2d(input_tmp[i], self.kernels[i, j], mode='valid')
         return output
     def backward(self, output_grad, learn_rate):
         kernel_grad = np.zeros(self.kernel_shape)
-        tmp_input_grad = np.zeros((self.depth, self.output_height - self.kernel_size + 1, self.output_width - self.kernel_size + 1))
-        for i in range(self.depth):
-            for j in range(self.output_depth):
+        tmp_input_grad = np.zeros((self.in_channels, self.output_height - self.kernel_size + 1, self.output_width - self.kernel_size + 1))
+        for i in range(self.in_channels):
+            for j in range(self.out_channels):
                 kernel_grad[i, j] = signal.correlate2d(self.input[i], output_grad[j], mode='valid')[::-1, ::-1]
                 tmp_input_grad[i] += signal.correlate2d(output_grad[j], self.kernels[i, j], mode='valid')
         input_grad = tmp_input_grad[:, ::self.kernel_size, ::self.kernel_size]
@@ -121,28 +110,28 @@ class ConvolutionalTranspose(Layer):
 
 # a convolutional layer that does not maintain the width and height of the data provided as input
 class ConvolutionalValid(Layer):
-    def __init__(self, input_shape, kernel_size, depth, directory=None):
+    def __init__(self, input_shape, kernel_size, out_channels, directory=None):
         self.input_shape = input_shape
-        self.input_depth = input_shape[0]
-        self.depth = depth
-        self.kernel_shape = (depth, self.input_depth, kernel_size, kernel_size)
+        self.in_channels = input_shape[0]
+        self.out_channels = out_channels
+        self.kernel_shape = (out_channels, self.in_channels, kernel_size, kernel_size)
         self.kernels = np.random.rand(*self.kernel_shape) - 0.5
-        self.biases = np.random.rand(depth, input_shape[1] - kernel_size + 1, input_shape[2] - kernel_size + 1) - 0.5
+        self.biases = np.random.rand(out_channels, input_shape[1] - kernel_size + 1, input_shape[2] - kernel_size + 1) - 0.5
         if directory is not None:
             self.kernels = np.load(directory + '/kernels.pickle', allow_pickle=True)
             self.biases = np.load(directory + '/biases.pickle', allow_pickle=True)
     def forward(self, input):
         self.input = input
-        output = np.copy(self.biases)
-        for i in range(self.depth):
-            for j in range(self.input_depth):
+        output = self.biases.copy()
+        for i in range(self.out_channels):
+            for j in range(self.in_channels):
                 output[i] += signal.correlate2d(self.input[j], self.kernels[i, j], mode='valid')
         return output
     def backward(self, output_grad, learn_rate):
         kernel_grad = np.zeros(self.kernel_shape)
         input_grad = np.zeros(self.input_shape)
-        for i in range(self.depth):
-            for j in range(self.input_depth):
+        for i in range(self.out_channels):
+            for j in range(self.in_channels):
                 kernel_grad[i, j] = signal.correlate2d(self.input[j], output_grad[i], mode='valid')
                 input_grad[j] += signal.convolve2d(output_grad[i], self.kernels[i, j], mode='full')
         self.kernels -= learn_rate * kernel_grad
@@ -159,22 +148,22 @@ class ConvolutionalValid(Layer):
 # a maxpool layer
 class MaxPool(Layer):
     def __init__(self, input_shape, pool_size):
-        input_depth, input_height, input_width = input_shape
+        input_channels, input_height, input_width = input_shape
         self.input_shape = input_shape
-        self.input_depth = input_depth
+        self.input_channels = input_channels
         self.input_height = input_height
         self.input_width = input_width
         self.pool_size = pool_size
-        self.output_shape = (input_depth, input_height // pool_size, input_width // pool_size)
+        self.output_shape = (input_channels, input_height // pool_size, input_width // pool_size)
     def forward(self, input):
         self.input = input
         output = np.zeros(self.output_shape)
-        for i in range(self.input_depth):
+        for i in range(self.input_channels):
             output[i] = self.input[i].reshape(self.input_height // self.pool_size, self.pool_size, self.input_width // self.pool_size, self.pool_size).max(axis=(1, 3))
         return output
     def backward(self, output_grad, learn_rate):
         input_grad = np.zeros(self.input_shape)
-        for i in range(self.input_depth):
+        for i in range(self.input_channels):
             input_grad[i] = np.repeat(np.repeat(output_grad[i], self.pool_size, axis=0), self.pool_size, axis=1) * (self.input[i] == np.repeat(np.repeat(self.output[i], self.pool_size, axis=0), self.pool_size, axis=1))
         return input_grad
 
@@ -184,18 +173,17 @@ class Reshape(Layer):
         self.input_shape = input_shape
         self.output_shape = output_shape
     def forward(self, input):
-        return np.reshape(input, self.output_shape)
+        return input.reshape(self.output_shape)
     def backward(self, output_grad, learn_rate):
-        return np.reshape(output_grad, self.input_shape)
+        return output_grad.reshape(self.input_shape)
 
 # a ReLU activation layer
-class ReLU(Activation):
-    def __init__(self, directory=None):
-        def relu_func(x):
-            return x * (x > 0)
-        def relu_derivative(x):
-            return np.ones_like(x) * (x > 0)
-        super().__init__(relu_func, relu_derivative)
+class ReLU(Layer):
+    def forward(self, input):
+        self.input = input
+        return input * (input > 0)
+    def backward(self, output_grad, learn_rate):
+        return output_grad * (np.ones_like(self.input) * (self.input > 0))
 
 
 # error function and its derivative
@@ -213,11 +201,11 @@ batch_size = 32
 
 
 # Whether or not to train or test if the code is run
-training = False
+training = True
 testing = False
 
 # Whether or not to run the segmenter
-segment = False
+segment = True
 
 
 # encoder
